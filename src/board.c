22e59dc9b1b96845c12c19eb9183c3c29f615e2e
Sami Allakkis <Sami@Samis-M1-MacBook-Pro.local>
2025-10-04 19:03:02 +0300

    Implementation of dropChecker and Win by Adam fixed

--- src/board.c
+++ src/board.c
@@ -2,38 +2,38 @@
 #include "../include/board.h"
 
 void createBoard(char board[ROWS][COLS]){
-	for(int i=0; i<ROWS; i++){
-		for(int j=0; j<COLS; j++){
-			board[i][j]='.';
-		}
-	}
+    for(int i=0; i<ROWS; i++){
+        for(int j=0; j<COLS; j++){
+            board[i][j]='.';
+        }
+    }
 }
 
 void printBoard(char board[ROWS][COLS]){
-	for(int i=0; i<ROWS; i++){
-		for(int j=0; j<COLS; j++){
-			printf("%c ", board[i][j]);
-		}
-		printf("\n");
-	}
+    for(int i=0; i<ROWS; i++){
+        for(int j=0; j<COLS; j++){
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
 
-	//Printing the column numbers
-	for(int j=0; j<COLS; j++){
-		printf("%d ", j+1);
-	}
-	printf("\n\n");
+    //Printing the column numbers
+    for(int j=0; j<COLS; j++){
+        printf("%d ", j+1);
+    }
+    printf("\n\n");
 }
 
 int dropChecker(char board[ROWS][COLS], int col, char player){
-	if(col<0 || col >= COLS) return 0;	//invalid input
+    if(col<0 || col >= COLS) return 0;  //invalid input
 
-	for(int i=ROWS-1; i>=0; i--){
-		if(board[i][col]=='.'){
-			board[i][col] = player;
-			return 1;
-		}
-	}
-	return 0;	//column is full
+    for(int i=ROWS-1; i>=0; i--){
+        if(board[i][col]=='.'){
+            board[i][col] = player;
+            return 1;
+        }
+    }
+    return 0;   //column is full
 }
 
 int checkWin(char board[ROWS][COLS], char player) {
