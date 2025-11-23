eabc7417df423dfed7f194063355b2f61dec0f07
Jamil-M03 <jamilm2006@gmail.com>
2025-11-23 18:17:13 +0200

    added support for multiplayer across same lan

--- src/main.c
+++ src/main.c
@@ -1,158 +1,163 @@
+// use flags -DSERVER for server and -DCLIENT for client
+// use flag -DBOT to play against bot
 #include "../include/board.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
-    srand(time(NULL));  // seed random once here
+#ifdef SERVER
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define PORT 9001
+#endif
 
+#ifdef CLIENT
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#define PORT 9001
+#endif
+
+#ifdef SERVER
+int start_server() {
+    int sockfd, clientfd;
+    struct sockaddr_in serv, cli;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    serv.sin_family = AF_INET;
+    serv.sin_addr.s_addr = INADDR_ANY;
+    serv.sin_port = htons(PORT);
+    bind(sockfd, (struct sockaddr*)&serv, sizeof(serv));
+    listen(sockfd, 1);
+    printf("Waiting for a client to connect...\n");
+    socklen_t clilen = sizeof(cli);
+    clientfd = accept(sockfd, (struct sockaddr*)&cli, &clilen);
+    printf("Client connected!\n");
+    return clientfd;
+}
+#endif
+
+#ifdef CLIENT
+int start_client(const char* server_ip) {
+    int sockfd;
+    struct sockaddr_in serv;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    serv.sin_family = AF_INET;
+    serv.sin_port = htons(PORT);
+    inet_pton(AF_INET, server_ip, &serv.sin_addr);
+    printf("Connecting to server...\n");
+    connect(sockfd, (struct sockaddr*)&serv, sizeof(serv));
+    printf("Connected to server!\n");
+    return sockfd;
+}
+#endif
+
+#ifdef SERVER
+void send_int(int sock, int value) { send(sock, &value, sizeof(value), 0); }
+int recv_int(int sock) { int val; recv(sock, &val, sizeof(val), 0); return val; }
+#endif
+
+#ifdef CLIENT
+void send_int(int sock, int value) { send(sock, &value, sizeof(value), 0); }
+int recv_int(int sock) { int val; recv(sock, &val, sizeof(val), 0); return val; }
+#endif
+
+int main() {
+    srand(time(NULL));
     char board[ROWS][COLS];
-    char playerA, playerB;
-    int col, turn = 0;
-    int moves=0;   // used for the draw
-    int opponent=-1;   //used to know if its human or a bot (and which one)
+    char playerA = 'A', playerB = 'B';
+    int col, turn = 0, moves = 0;
 
-    printf("Welcome to Connect Four!\n");
-
-    //Ask players whether they want to play against a human or a bot
-    int valid_choices[] = {0, 1, 2, 3};
-    int found;
-
-    while (1) {
-        printf("Choose your opponent:\n  0 - Play against a human\n  1 - Play against the easy-level bot\n  2 - Play against the medium-level bot\n  3 - Play against the hard-level bot\nEnter your choice: ");
-        if (scanf("%d", &opponent) != 1) {
-            printf("Invalid input. Please type 0, 1, 2, or 3.\n");
-                while (getchar() != '\n'); // clear invalid input
-                continue;
-        }
-
-        while (getchar() != '\n'); // clear newline
-
-        // check if input is in the list
-        found = 0;
-        for (int i = 0; i < 4; i++) {
-            if (opponent == valid_choices[i]) {
-                found = 1;
-                break;
-            }
-        }
-
-        if (found) break; // valid input, exit loop
-        printf("Invalid choice. Please enter 0, 1, 2, or 3.\n");
-    }
-
-    //Ask players for their desired symbols
-    printf("Player A:");
-    scanf(" %c", &playerA);
-
-    //Setting playerB's name in case of a human playing
-    if(opponent==0){
-        printf("Player B:");
-        scanf(" %c", &playerB);
-        while(getchar() != '\n');   //clear leftover input
-    }
-    else{
-        playerB = 'B';
-        if (playerA == 'B') playerB = 'O';  // Avoid duplicate symbol
-    }
-
-    printf("\n");
-
-    //Creating an empty board
     createBoard(board);
     printBoard(board);
 
-    // will exit on breaks
-    while(1){
+#ifdef BOT
+    int opponent = 1; // Bot mode
+#elif defined(SERVER) || defined(CLIENT)
+    int sock;
+#endif
 
-        if (turn == 0) {
-            // Player A (always human)
-            printf("Player A (%c), choose a column (1-%d): ", playerA, COLS);
-            if (scanf("%d", &col) != 1) {
-                printf("Invalid input.\n");
-                while(getchar() != '\n');
-                continue;
-            }
+#ifdef SERVER
+    sock = start_server();
+#elif defined(CLIENT)
+    char server_ip[16];
+    printf("Enter server IP: ");
+    scanf("%15s", server_ip);
+    sock = start_client(server_ip);
+#endif
+
+    while (1) {
+        char currentPlayer = (turn == 0) ? playerA : playerB;
+
+#ifdef BOT
+        if (turn == 1) {
+            // Bot move
+            col = bot1(board);
+            printf("Bot chooses column %d\n", col + 1);
+        } else {
+            printf("Player %c, choose a column (1-%d): ", currentPlayer, COLS);
+            if (scanf("%d", &col) != 1) { while(getchar() != '\n'); continue; }
             while(getchar() != '\n');
-            col--;
         }
-
-        else {
-            // Player B
-            if (opponent == 0) {
-                printf("Player B (%c), choose a column (1-%d): ", playerB, COLS);
-                if (scanf("%d", &col) != 1) {
-                    printf("Invalid input.\n");
-                    while(getchar() != '\n');
-                    continue;
-                }
-                while(getchar() != '\n');
-                col--;
-            }
-
-            else if(opponent == 1){
-                // Bot1 mode
-                col = bot1(board);
-                printf("Bot1 chose column %d\n", col + 1);
-            }
-            else if(opponent == 2){
-                // Bot2 mode
-                col = bot2(board, moves);
-                printf("Bot2 chose column %d\n", col + 1);
-            }
-            else{
-                // Bot2 mode
-                col = bot3(board, moves, playerA);
-                printf("Bot3 chose column %d\n", col + 1);
-            }
+#elif defined(SERVER)
+        if (turn == 0) {
+            // server human
+            printf("Player %c, choose a column (1-%d): ", currentPlayer, COLS);
+            if (scanf("%d", &col) != 1) { while(getchar() != '\n'); continue; }
+            while(getchar() != '\n');
+            send_int(sock, col);
+        } else {
+            // receive move from client
+            printf("Waiting for client move...\n");
+            col = recv_int(sock);
+            printf("Client chose column %d\n", col + 1);
         }
+#elif defined(CLIENT)
+        if (turn == 0) {
+            // receive move from server
+            printf("Waiting for server move...\n");
+            col = recv_int(sock);
+            printf("Server chose column %d\n", col + 1);
+        } else {
+            // client human
+            printf("Player %c, choose a column (1-%d): ", currentPlayer, COLS);
+            if (scanf("%d", &col) != 1) { while(getchar() != '\n'); continue; }
+            while(getchar() != '\n');
+            send_int(sock, col);
+        }
+#else
+        // Local two-player mode
+        printf("Player %c, choose a column (1-%d): ", currentPlayer, COLS);
+        if (scanf("%d", &col) != 1) { while(getchar() != '\n'); continue; }
+        while(getchar() != '\n');
+#endif
 
-
-        if(turn==0){
-            // Player A drops checker
-            if(col<0 || col>=COLS){
-                printf("Invalid column. Choose a number between 1 and %d.\n", COLS);
-                continue;
-            }
-            if(!dropChecker(board, col, playerA)){
-                printf("Column %d is full. Try another column.\n", col+1);
-                continue;
-            }
-
+        if (dropChecker(board, col, currentPlayer)) {
             moves++;
             printBoard(board);
-            if(checkWin(board, col, playerA)){
-                printf("\nPlayer A (%c) wins!\n\n", playerA);
+            if (checkWin(board, col, currentPlayer)) {
+                printf("Player %c wins!\n", currentPlayer);
+#ifdef SERVER
+                close(sock);
+#endif
+#ifdef CLIENT
+                close(sock);
+#endif
                 break;
             }
-            if(moves == ROWS * COLS){
-                printf("It's a tie! The board is full.\n");
+            if (moves == ROWS*COLS) {
+                printf("It's a tie! Board full.\n");
+#ifdef SERVER
+                close(sock);
+#endif
+#ifdef CLIENT
+                close(sock);
+#endif
                 break;
             }
-            turn = 1 - turn;  // switch to player B
-        }
-
-        else{
-            // Player B drops checker
-            if(col<0 || col>=COLS){
-                printf("Invalid column. Choose a number between 1 and %d.\n", COLS);
-                continue;
-            }
-            if(!dropChecker(board, col, playerB)){
-                printf("Column %d is full. Try another column.\n", col+1);
-                continue;
-            }
-            moves++;
-            printBoard(board);
-            if(checkWin(board, col, playerB)){
-                printf("\nPlayer B (%c) wins!\n\n", playerB);
-                break;
-            }
-            if(moves == ROWS * COLS){
-                printf("It's a tie! The board is full.\n");
-                break;
-            }
-            turn = 1 - turn;  // switch to player A
+            turn = 1 - turn;
+        } else {
+            printf("Column %d is full. Try another column.\n", col+1);
         }
     }
     return 0;
